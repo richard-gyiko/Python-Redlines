4eb2d7309606a47b109963f060a2f35d5c89ddff
Richard Gyiko <richard.gyiko@whizystems.com>
2025-04-25 11:16:28 +0200

    Disable shell on osx build

--- build_hooks.py
+++ build_hooks.py
@@ -70,7 +70,7 @@ class HatchRunBuildHook(BuildHookInterface):
                 out_dir,
             ],
             check=True,
-            shell=True
+            shell=not target_platform.startswith("osx"),
         )
 
         # Use force_include to include the binaries in the wheel
