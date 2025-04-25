d14dd2af0ba1b2261f995ef4eac373c99ce1c5b1
Richard Gyiko <richard.gyiko@whizystems.com>
2025-04-25 11:06:41 +0200

    Run dotnet build in shell

--- build_hooks.py
+++ build_hooks.py
@@ -70,6 +70,7 @@ class HatchRunBuildHook(BuildHookInterface):
                 out_dir,
             ],
             check=True,
+            shell=True
         )
 
         # Use force_include to include the binaries in the wheel
